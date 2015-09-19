2713f814b53661b0aba6960346be3f5ba7e24203
s.maini <s.maini@masterhouse.it>
2015-09-19 17:59:08 +0200

    proper sound , unclamped textures

--- main.c
+++ main.c
@@ -14,25 +14,20 @@ jack_port_t *input_port;
 jack_port_t *output_port;
 jack_client_t *client;
 
-int windoww = 256, windowh = 256, drumw = 256, drumh = 256, mousel = GL_FALSE, flip=0;
+int windoww = 256, windowh = 256, drumw = 128, drumh = 128, mousel = GL_FALSE, flip=0;
 float mousec[4]={1.0, 1.0, 1.0, 1.0};
 
 GLFWwindow* window;
 
-const GLenum attachments[3]={GL_COLOR_ATTACHMENT0,GL_COLOR_ATTACHMENT1,GL_COLOR_ATTACHMENT2};
+const GLenum attachments[3]={GL_COLOR_ATTACHMENT0,GL_COLOR_ATTACHMENT1,GL_FRONT};
 
 double mousex = 0.0;
 double mousey = 0.0;
-
-int SAMPLERATE=44100;
-int BUFSIZE=16384;
-int SMPSIZE=4096;
 
 GLuint tex1;
 GLuint tex2;
 GLuint texs;
 GLuint fb;
-GLuint pb;
 GLuint drawlist;
 
 GLuint rprogram;
@@ -110,22 +105,13 @@ GLuint createtexture (int w, int h)
     // or the following two parameters can be used if there are none)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_FLOAT, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, w, h, 0, GL_RGBA, GL_FLOAT, 0);
     printf("glTexImage2D %d\r\n",glGetError());
     return texture;
 }
 
-GLuint createpb(void)
-{
-    GLuint pb;
-    glGenBuffers(1, &pb);
-    glBindBuffer(GL_PIXEL_PACK_BUFFER, pb);
-    glBufferData(GL_PIXEL_PACK_BUFFER, BUFSIZE, NULL, GL_DYNAMIC_READ);
-    glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
-    return pb;
-}
 
-GLuint createfb(GLuint tex1, GLuint tex2, GLuint texs)
+GLuint createfb(GLuint tex1, GLuint tex2)
 {
     GLuint fb;
     glGenFramebuffers(1, &fb);
@@ -136,17 +122,12 @@ GLuint createfb(GLuint tex1, GLuint tex2, GLuint texs)
     printf("glFramebufferTexture2D %d\r\n",glGetError());
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, tex2, 0);
     printf("glFramebufferTexture2D %d\r\n",glGetError());
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, texs, 0);
-    printf("glFramebufferTexture2D %d\r\n",glGetError());
     printf("glCheckFramebufferStatus %d\r\n",glCheckFramebufferStatus(GL_FRAMEBUFFER));
-    glClearColor(0.0, 0.0, 1.0, 1.0);
+    glClearColor(0.0, 0.0, 0.9999, 1.0);
     glDrawBuffers(1,&attachments[0]);
     printf("glDrawBuffers %d\r\n",glGetError());
     glClear(GL_COLOR_BUFFER_BIT);
     glDrawBuffers(1,&attachments[1]);
-    printf("glDrawBuffers %d\r\n",glGetError());
-    glClear(GL_COLOR_BUFFER_BIT);
-    glDrawBuffers(1,&attachments[2]);
     printf("glDrawBuffers %d\r\n",glGetError());
     glClear(GL_COLOR_BUFFER_BIT);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
@@ -183,15 +164,14 @@ void reshade(int w, int h)
     fprogram = createprogram(v2,f,w,h);
 }
 
-void prepare(int w, int h)
+void prepare(int w, int h, int smpsize)
 {
     glEnable(GL_TEXTURE_2D);
     tex1 = createtexture(w,h);
     tex2 = createtexture(w,h);
-    texs = createtexture(w,h);
+    texs = createtexture(smpsize,1);
     reshade(w,h);
-    fb = createfb(tex1,tex2,texs);
-    pb = createpb();
+    fb = createfb(tex1,tex2);
     drawlist = createlist();
 }
 
@@ -202,41 +182,10 @@ void close_callback(GLFWwindow *window)
 void size_callback(GLFWwindow *window, int w, int h)
 {
     windoww = w; windowh = h;
-
-    pthread_mutex_lock(&lock);
-    glfwMakeContextCurrent(window);
-
-    glViewport(0,0,w,h);
-
-    glfwMakeContextCurrent(NULL);
-    pthread_mutex_unlock(&lock);
 }
 void mouse_callback(GLFWwindow *window, int b, int a, int m)
 {
-    if (b==GLFW_MOUSE_BUTTON_LEFT && a==GLFW_PRESS)
-    {
-        mousel=GL_TRUE;
-
-        pthread_mutex_lock(&lock); glGetError();
-        glfwMakeContextCurrent(window);
-
-        int x = (int)((mousex * drumw) / windoww);
-        int y = drumh - (int)((mousey * drumh) / windowh) - 1;
-
-        glBindTexture(GL_TEXTURE_2D, tex2);
-        glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, 1, 1, GL_RGBA, GL_FLOAT, mousec);
-
-        glUseProgram(rprogram);
-        glCallList(drawlist);
-
-        glFlush();
-        glfwSwapBuffers(window);
-
-        glfwMakeContextCurrent(NULL);
-        pthread_mutex_unlock(&lock);
-
-
-    }
+    if (b==GLFW_MOUSE_BUTTON_LEFT && a==GLFW_PRESS) mousel=GL_TRUE;
     if (b==GLFW_MOUSE_BUTTON_LEFT && a==GLFW_RELEASE) mousel=GL_FALSE;
 }
 void cursor_callback(GLFWwindow *window, double x, double y)
@@ -258,13 +207,13 @@ int process (jack_nframes_t nframes, void *arg)
     out = jack_port_get_buffer (output_port, nframes);
 
     pthread_mutex_lock(&lock);
-    glfwMakeContextCurrent(window); glGetError();
+    glfwMakeContextCurrent(window);
 
     glViewport(0, 0, drumw, drumh);
     glBindFramebuffer(GL_FRAMEBUFFER,fb);
     glUseProgram(fprogram);
 
-    for(int i=0;i<2;i++,flip^=1)
+    for(int i=0;i<nframes;i++,flip^=1)
     {
         if(flip)
         {
@@ -277,25 +226,15 @@ int process (jack_nframes_t nframes, void *arg)
             glBindTexture(GL_TEXTURE_2D,tex2);
         }
         glCallList(drawlist);
+
+        glBindTexture(GL_TEXTURE_2D,texs);
+        glCopyTexSubImage2D(GL_TEXTURE_2D,0,i,0,0,0,1,1);
     }
+    glBindFramebuffer(GL_FRAMEBUFFER,0);
 
     // SOUND BLOCK
-
-    //glBindBuffer(GL_PIXEL_PACK_BUFFER,pb);
-
-    //glReadBuffer(attachments[1]);
-    //printf("glReadBuffers %d\r\n",glGetError());
-    glReadPixels(0, 0, drumw, nframes/drumw, GL_RED, GL_FLOAT, out);
-    //glReadBuffer(GL_FRONT);
-
-
-    //glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
-    //glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
-    //glBindBuffer(GL_PIXEL_PACK_BUFFER,0);
-
-
-    glBindFramebuffer(GL_FRAMEBUFFER,0);
-    glViewport(0, 0, windoww, windowh);
+    glGetTexImage(GL_TEXTURE_2D,0,GL_RED,GL_FLOAT,out);
+    //glReadPixels(0, 0, drumw, nframes/drumw, GL_RED, GL_FLOAT, out);
 
     glfwMakeContextCurrent(NULL);
     pthread_mutex_unlock(&lock);
@@ -308,15 +247,6 @@ int process (jack_nframes_t nframes, void *arg)
 
 int main(int argc, char **argv)
 {
-    if(argc>=3)
-    {
-        windoww = atoi(argv[1]);
-        windowh = atoi(argv[2]);
-    }
-
-    drumw = windoww;
-    drumh = windowh;
-
     pthread_mutex_init(&lock, NULL);
 
     glfwInit();
@@ -324,6 +254,10 @@ int main(int argc, char **argv)
 
     glfwMakeContextCurrent(window);
     glewInit();
+
+    glClampColor(GL_CLAMP_READ_COLOR, GL_FALSE);
+    glClampColor(GL_CLAMP_VERTEX_COLOR, GL_FALSE);
+    glClampColor(GL_CLAMP_FRAGMENT_COLOR, GL_FALSE);
 
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -335,10 +269,13 @@ int main(int argc, char **argv)
     glfwSetCursorPosCallback(window,&cursor_callback);
     glfwSwapInterval(2);
 
-    prepare(drumw,drumh);
-
     jack_status_t status;
     client = jack_client_open ("brumdrum", JackNullOption, &status, NULL);
+
+    prepare(drumw,drumh,jack_get_buffer_size(client));
+
+    glfwMakeContextCurrent(NULL);
+
     jack_set_thread_init_callback(client, jack_init, 0);
     jack_set_process_callback (client, process, 0);
     jack_on_shutdown (client, jack_shutdown, 0);
@@ -357,34 +294,33 @@ int main(int argc, char **argv)
 
     while(glfwWindowShouldClose(window)==GL_FALSE)
     {
-        glfwWaitEvents();
+        glfwPollEvents();
 
-        /*
-
-        // RENDER BLOCK
         pthread_mutex_lock(&lock);
         glfwMakeContextCurrent(window);
 
-        glfwPollEvents();
-
-        if(mousel)
+        if(mousel==GL_TRUE)
         {
+            mousel=GL_FALSE;
+
             int x = (int)((mousex * drumw) / windoww);
             int y = drumh - (int)((mousey * drumh) / windowh) - 1;
 
-            glBindTexture(GL_TEXTURE_2D, tex1);
-            glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, 1, 1, GL_RGBA, GL_FLOAT, mousec);
+            glBindTexture(GL_TEXTURE_2D, tex2);
+            glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, 1, 1, GL_RED, GL_FLOAT, mousec);
         }
 
+        glViewport(0, 0, windoww, windowh);
         glUseProgram(rprogram);
-        blit(tex1,drawlist);
+        glBindTexture(GL_TEXTURE_2D, tex2);
+        glCallList(drawlist);
+
+        glFlush();
+        glfwSwapBuffers(window);
 
         glfwMakeContextCurrent(NULL);
         pthread_mutex_unlock(&lock);
 
-        glfwSwapBuffers(window);
-
-         */
     }
     jack_client_close(client);
 
@@ -393,7 +329,6 @@ int main(int argc, char **argv)
     pthread_mutex_destroy(&lock);
 
     return 0;
-
 
 }
 
